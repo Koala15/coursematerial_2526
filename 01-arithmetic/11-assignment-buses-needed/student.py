631271088d0d7694697c5109c2d77e4e9fb169a2
Koala15 <oana.stoica@student.ucll.be>
2025-10-02 11:55:32 +0200

    introduction completed + arithmetic 1/2 completed

--- 01-arithmetic/11-assignment-buses-needed/student.py
+++ 01-arithmetic/11-assignment-buses-needed/student.py
@@ -1,1 +1,4 @@
-# write your code here
+from math import ceil
+def buses_needed(people_count, bus_capacity):
+    amount_buses = people_count / bus_capacity
+    return ceil(amount_buses)
--- 01-arithmetic/12-assignment-pizza/student.py
+++ 01-arithmetic/12-assignment-pizza/student.py
@@ -1,1 +1,4 @@
-# write your code here
+from math import ceil
+def pizza(n_people, slices_per_pizza):
+    amount_pizza = ceil(n_people / slices_per_pizza)
+    return amount_pizza
